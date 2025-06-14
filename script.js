9986ac5a57fd6d558de4e1e6a2befdc7ae0f3d3e
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-06-14 17:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, idx) => {
-  input.addEventListener("input", () => {
-    if (input.value.length === 1 && idx < inputs.length - 1) {
+  input.addEventListener("input", (e) => {
+    // Allow only digits
+    const value = e.target.value.replace(/[^0-9]/g, '');
+    e.target.value = value;
+
+    if (value && idx < inputs.length - 1) {
       inputs[idx + 1].focus();
     }
   });
 
   input.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace" && input.value === "") {
-      if (idx > 0) {
-        inputs[idx - 1].focus();
-        inputs[idx - 1].value = "";
-      }
+    if (e.key === "Backspace" && !input.value && idx > 0) {
+      inputs[idx - 1].focus();
+      inputs[idx - 1].value = "";
     }
   });
 });
+
